d984c3d1247299f96c1339517d8aa1b4c687857c
jutin0852 <jutindikonu8@gmail.com>
2022-10-10 11:49:24 +0100

    made card inputs update in real time

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 var cardForm=document.forms['card'];
+//**************card inputs
+  function numInput(){
+    var cardNumber=document.getElementById('number').value;
+    document.getElementById("cardnum").textContent=cardNumber;   
+}
+function nameInput(){
+    var cardName=document.getElementById("name").value;
+    document.getElementById("cardname").textContent=cardName;
+}
+var cvc=cardForm['cvc'];
+cvc.addEventListener('input',function(){
+   var cvvvalue= cvc.value;
+   document.getElementById('cvv').textContent=cvvvalue;
+
+})
+function monthInput(){
+    var cardMonth=document.getElementById("month").value;
+    document.getElementById("M").textContent=cardMonth;
+}
+function yearInput(){
+    var cardYear=document.getElementById('year').value;
+    document.getElementById('Y').textContent=cardYear;
+}
